d2a04c1944e778f1cb5fed9e53c44cfe446174ba
DeabitTech <info@infomillennium2000.it>
2021-12-30 16:56:18 +0100

    gsdg

--- db/controller/records-ctrl.js
+++ db/controller/records-ctrl.js
@@ -83,7 +83,7 @@ createOrUpdateUserTokenList = async (req,res) => {
     }
     if(userFinded.tokenList[body.chainId]){
         
-       Array(userFinded.tokenList[body.chainId]).find((tokenAddress,index)=>{
+       Array(userFinded.tokenList[body.chainId]).map().find((tokenAddress,index)=>{
            String(tokenAddress[index]).toLocaleLowerCase() === String(body.tokenList[body.chainId][index].toLocaleLowerCase()) ?  
                 console.log("sono tutti uguali") :
                 console.log("non sono tutti uguali"); 
