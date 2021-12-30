29c1e058aa21ed126b8fef35a148ff7792ac959a
DeabitTech <info@infomillennium2000.it>
2021-12-30 16:53:33 +0100

    wef

--- db/controller/records-ctrl.js
+++ db/controller/records-ctrl.js
@@ -84,7 +84,7 @@ createOrUpdateUserTokenList = async (req,res) => {
     if(userFinded.tokenList[body.chainId]){
         
        Array(userFinded.tokenList[body.chainId]).find((tokenAddress,index)=>{
-           String(tokenAddress[index]).toLocaleLowerCase() === String(body.tokenList[body.chainId][index]) ?  
+           String(tokenAddress[index]).toLocaleLowerCase() === String(body.tokenList[body.chainId][index].toLocaleLowerCase()) ?  
                 console.log("sono tutti uguali") :
                 console.log("non sono tutti uguali"); 
        }) 
