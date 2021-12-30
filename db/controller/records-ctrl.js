e19a88a834dcc13ddbafdf0f0d0ff445258dd9ea
DeabitTech <info@infomillennium2000.it>
2021-12-30 15:54:58 +0100

    token list section

--- db/controller/records-ctrl.js
+++ db/controller/records-ctrl.js
@@ -1,6 +1,6 @@
 const {getCollection} = require('../dataModels/dataModel')
 
-createUser = async (req,res) => {
+createOrUpdateUser = async (req,res) => {
 	console.log('sono entrato in create User',req.body)
 	const body = req.body;
 	const listRecord = [];
@@ -17,7 +17,6 @@ createUser = async (req,res) => {
     const userFinded = await collection.findOneAndUpdate({ address: body.address },
                                         { $set: { lastLogin: body.lastLogin } })
     if(userFinded.value){
-        console.log("o qui??", userFinded)
         return res.status(201).json({
             success:true,
             id:req._id,
@@ -25,11 +24,9 @@ createUser = async (req,res) => {
         })
     }
     else{
-        console.log("qui??", userFinded)
-
+        
         await collection.insertMany(listRecord,{safe:true},(err,resp)=>{
-            if(!err){
-                
+            if(!err){              
                 return res.status(201).json({
                     success: true,
                     id: req._id,
@@ -37,7 +34,6 @@ createUser = async (req,res) => {
                 })
             }
             else{
-               console.log("ecco l errore",err);
                 return res.status(400).json({
                     err,
                     message: `${body.address} non creato!`,
@@ -47,11 +43,37 @@ createUser = async (req,res) => {
         })		
     }
 	
-
-
 }
 
+createOrUpdateUserTokenList = async (req,res) => {
+    console.log('sono entrato in create User',req.body)
+	const body = req.body;
+	const listRecord = [];
+	listRecord.push(body);
+	
+	if(!body.address) {
+		return res.status(400).json({
+				success:false,
+				error:'body mancante',
+			})
+	}
+	const collection = await getCollection('Users');
+    console.log("arriva fin qui");
+    const userFinded = await collection.findOneAndUpdate({ address: body.address },
+                                        { $set: { tokenList: body.tokenList } })
+    if(userFinded.value){
+        return res.status(201).json({
+            success:true,
+            id:req._id,
+            message: `${body.address} aggiornato`,
+        })
+    }
+}
+
+
+
 module.exports = {
-	createUser
+	createOrUpdateUser,
+    createOrUpdateUserTokenList
 
 }
--- db/routes/records-router.js
+++ db/routes/records-router.js
@@ -4,6 +4,7 @@ const RecordsCtrl = require('../controller/records-ctrl')
 
 const router = express.Router()
 
-router.post('/createUser', RecordsCtrl.createUser)
+router.post('/createOrUpdateUser', RecordsCtrl.createOrUpdateUser)
+router.post('/createOrUpdateUserTokenList', RecordsCtrl.createOrUpdateUserTokenList)
 
 module.exports = router
