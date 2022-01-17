f5d6a1e1197bfe536f16332153d4617cf39f6933
DeabitTech <info@infomillennium2000.it>
2022-01-17 17:33:03 +0100

    trade action

--- db/controller/records-ctrl.js
+++ db/controller/records-ctrl.js
@@ -61,6 +61,7 @@ updateUserTokenList = async (req,res) => {
 				error:'body mancante',
 			})
 	}
+
 	const collection = await getCollection('Users');
     const userFinded = await collection.findOne({ address: body.address })
     if(!userFinded.tokenList){
@@ -132,11 +133,74 @@ updateUserTokenList = async (req,res) => {
 }
 
 
+insertTrades = async (req,res) => {
+    const body = req.body;
+	const listRecord = [];
+	listRecord.push(body);
+
+    if(!body.address) {
+		return res.status(400).json({
+				success:false,
+				error:'body mancante',
+			})
+	}
+	const collection = await getCollection('Trades');
+
+    let mock_trade = {
+        txId: "0xd313f213c405b9fbf0da9274842d5fdd0db9f6f15e15c4eb7221dd2d74611da0",
+        user: "0x545d1561bb5eC7CF96a36Fb145D3B5928419142f",
+        tokenFrom:"0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+        tokenTo:"0x3EE2200Efb3400fAbB9AacF31297cBdD1d435D47",
+        amountIn:1000000000000000,
+        amountSwapped:90000,
+        priceFrom:1000000,
+        priceTo:20000,
+        status:true,
+        timestamp:new Date().now()
+
+
+
+    }
+
+    const tradeFinded = await collection.findOne({tokenFrom:mock_trade.tokenFrom, tokenTo:mock_trade.tokenTo})
+
+    /*let tradeFindedUpdated  ={
+        ...tradeFinded, 
+        tradeFinded[mock_trade.amountIn]:tradeFinded.amountIn  += mock_trade.amountIn,
+        amountSwapped:tradeFinded.amountSwapped += mock_trade.amountSwapped,
+
+    
+    }*/
+
+    if(!tradeFinded){
+        await collection.insertMany(listRecord,{safe:true},(err,resp)=>{
+            if(!err){              
+                return res.status(201).json({
+                    created:true,
+                    id: req._id,
+                    message: `${body.address} , ${JSON.stringify(resp)} creato!`,
+                    data: body
+                })
+            }
+            else{
+                return res.status(400).json({
+                    err,
+                    message: `${body.address} non creato!`,
+                })
+    
+            }
+        })		
+    }
+
+}
+
+
 
 
 
 module.exports = {
 	createOrUpdateUser,
-    updateUserTokenList
+    updateUserTokenList,
+    insertTrades
 
 }
--- db/routes/records-router.js
+++ db/routes/records-router.js
@@ -6,5 +6,6 @@ const router = express.Router()
 
 router.post('/createOrUpdateUser', RecordsCtrl.createOrUpdateUser)
 router.post('/updateUserTokenList', RecordsCtrl.updateUserTokenList)
+router.post('/insertTrade',RecordsCtrl.insertTrades)
 
 module.exports = router
