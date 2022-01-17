184153c260bc6b831bc281bd94eeb7e154b3afb5
DeabitTech <info@infomillennium2000.it>
2022-01-17 19:31:20 +0100

    srg

--- db/routes/records-router.js
+++ db/routes/records-router.js
@@ -7,5 +7,6 @@ const router = express.Router()
 router.post('/createOrUpdateUser', RecordsCtrl.createOrUpdateUser)
 router.post('/updateUserTokenList', RecordsCtrl.updateUserTokenList)
 router.post('/insertOrUpdateTrade',RecordsCtrl.insertOrUpdateTrades)
+router.post('/getTrades',RecordsCtrl.getTrades)
 
 module.exports = router
