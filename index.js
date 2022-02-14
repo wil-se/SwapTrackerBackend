fe6dc45a44cd3b4765b531eceaa791d82972f325
unknown <filippo.sofi.res@gmail.com>
2022-02-15 00:40:46 +0100

    init

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-require('dotenv').config({
-	path: '/home/ubuntu/SwapTrackerBackend/.env'
-});
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
+const http = require('http');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
@@ -12,24 +7,19 @@ const app = express();
 const recordsRouter = require('./db/routes/records-router');
 
 const PORT = 3333;
-const KEY = "/etc/letsencrypt/live/dev.swaptracker.io-0001/privkey.pem";
-const CERT = "/etc/letsencrypt/live/dev.swaptracker.io-0001/fullchain.pem"
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-	origin: '*'
-}));
+app.use(cors());
+
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.get('/', (req, res) => {
 	res.send('Sto runnando...... ')
 })
 app.use('/data', recordsRouter)
 
-https.createServer({
-    key: fs.readFileSync(KEY),
-    cert: fs.readFileSync(CERT),
-    passphrase: 'swaptracker'
-}, app).listen(PORT);
+http.createServer(app).listen(PORT);
