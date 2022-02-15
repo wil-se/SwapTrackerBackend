efdfb9a46ce72cdd777d4d507ddc9fbcc6892e28
unknown <filippo.sofi.res@gmail.com>
2022-02-15 13:58:19 +0100

    allow origin *

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 const recordsRouter = require('./db/routes/records-router');
 
 const PORT = 3000;
@@ -12,7 +32,7 @@ console.log("init api")
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({origin: '*'}));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({
