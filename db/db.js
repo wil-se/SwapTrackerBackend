e20ade57742dda5a0cb11fade5065f08605b3d51
unknown <filippo.sofi.res@gmail.com>
2022-02-15 12:50:17 +0100

    newdb connection

--- db/db.js
+++ db/db.js
@@ -1,18 +1,41 @@
 require('dotenv').config({ path: `${__dirname}/../.env`})
 
 const {MongoClient} = require('mongodb')
+
+let mongoClient = null;
+let cachedDb = null;
+
 const connectMongodb = async () =>{
- 
-    const client = await MongoClient.connect(process.env.DB_URL,{useNewUrlParser: true,useUnifiedTopology: true})
-        .catch(error => console.log(error))
+    
+    if (cachedDb && await mongoClient.isConnected()) {
+        console.info("cached db connection established");
+        return cachedDb;
+    }
 
-    const db = await client.db('Cluster0');
-    
-    return db
+    mongoClient = new MongoClient(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        poolSize: 10
+    });
 
-    
-    
-    
+    try{
+        await mongoClient.connect();
 
+        if ( ! (await mongoClient.isConnected())) {
+            await mongoClient.connect(err => {
+              if (err) {
+                console.error("Error : ", err);
+                throw new Error(`Unable to connect MongoDb Client, Reason: ${err}`
+                );
+              }
+            });
+        }
+        cachedDb = await client.db('Cluster0');
+        console.info("db connection established");
+        return cachedDb;
+    }catch(err){
+        console.log("Database error: %s", err);
+    }
 }
+
 module.exports = {connectMongodb}
