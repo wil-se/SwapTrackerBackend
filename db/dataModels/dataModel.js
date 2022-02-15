212cee1e846227f2d7af40943b08c90b87b842c4
unknown <filippo.sofi.res@gmail.com>
2022-02-15 02:44:37 +0100

    try catch

--- db/dataModels/dataModel.js
+++ db/dataModels/dataModel.js
@@ -3,10 +3,14 @@ const {connectMongodb} = require('../db');
 const getCollection = async(nameCollection) =>{
   try{
     const db = await connectMongodb()
+  }catch(err){
+    return 'CANNOT CONNECT'
+  }
+  try{
     const collection = await db.collection(nameCollection)
     return collection;
   }catch(err){
-    return err;
+    return 'CANNOT GET COLLECTION';
   }
 }
 
