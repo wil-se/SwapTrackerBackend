cdbb34098857ab1301c6a8156bf47130be908a3f
unknown <filippo.sofi.res@gmail.com>
2022-02-21 19:04:27 +0100

    gf

--- fetch-prices.js
+++ fetch-prices.js
@@ -4,53 +4,54 @@ const {getCollection} = require('./db/dataModels/dataModel')
 const axios = require('axios').default;
 
 const run = async () => {
+
   try{
     const prices = await getCollection("FiatPrices");
   
-  var symbols = [
-      "USD",
-      "EUR",
-      "CNY",
-      "INR",
-      "CAD",
-      "GBP",
-      "JPY",
-      "RUB",
-      "MXN",
-      "CHF",
-      "KRW",
-      "TRY",
-      "BRL",
-      "SEK",
-      "HKD",
-      "AUD",
-      "NOK",
-      "SGD",
-      "BTC",
-  ]
+    var symbols = [
+        "USD",
+        "EUR",
+        "CNY",
+        "INR",
+        "CAD",
+        "GBP",
+        "JPY",
+        "RUB",
+        "MXN",
+        "CHF",
+        "KRW",
+        "TRY",
+        "BRL",
+        "SEK",
+        "HKD",
+        "AUD",
+        "NOK",
+        "SGD",
+        "BTC",
+    ]
 
-  var hostname = 'data.fixer.io';
-  var path = `/api/latest?access_key=${process.env.API_KEY}&base=USD&symbols=${symbols.join(",")}&format=1`;
+    var hostname = 'data.fixer.io';
+    var path = `/api/latest?access_key=${process.env.API_KEY}&base=USD&symbols=${symbols.join(",")}&format=1`;
 
-  axios.get(`https://${hostname}${path}`)
-  .then(function (data) {
-    for(var rate in data['data']['rates']){
-      const query = {currency: rate};
-      const update = { $set: { currency: rate, timestamp: data['data']['timestamp'], date: data['data']['date'], rate: data['data']['rates'][rate] }};
-      const options = {upsert: true};
-      prices.updateOne(query, update, options);
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-    process.exit(1);
-  })
-  .then(function(){
-    process.exit(0);
-  })
-} catch(e){
-  console.log(e);
-}
+    axios.get(`https://${hostname}${path}`)
+    .then(function (data) {
+      for(var rate in data['data']['rates']){
+        const query = {currency: rate};
+        const update = { $set: { currency: rate, timestamp: data['data']['timestamp'], date: data['data']['date'], rate: data['data']['rates'][rate] }};
+        const options = {upsert: true};
+        await prices.updateOne(query, update, options);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      process.exit(1);
+    })
+    .then(function(){
+      process.exit(0);
+    })
+  } catch(e){
+    console.log(e);
+  }
 }
 
 
